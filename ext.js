24f8da435a1661f8044c231b6f4abb0f468901a9
SonawaneKuldeep8494 <99857060+SonawaneKuldeep29@users.noreply.github.com>
2022-04-27 18:28:11 -0700

    Add files via upload

---
+++ ext.js
@@ -0,0 +1,12 @@
+function test()
+                {
+                    var a = window.confirm("Your choice?");
+                    if(a)
+                        {
+                            alert("Yes");
+                        }
+                    else
+                        {
+                            alert("No");
+                        }
+                }
